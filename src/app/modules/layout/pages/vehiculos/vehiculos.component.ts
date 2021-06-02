1eeaa645cd5996ef85bfda030e78c21b495ebd8a
Octavio Herrera <octavioherreraing@gmail.com>
2021-06-02 13:41:37 -0600

    OCTAVIO: vehiculos

--- src/app/modules/layout/pages/vehiculos/vehiculos.component.ts
+++ src/app/modules/layout/pages/vehiculos/vehiculos.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { VehiculosService,ObjectService, MarcasService } from 'src/app/services/services.index';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators
+} from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import {
+  IMarcas,
+  IEstados,
+  IVehiculos,Vehiculos, IConcesionario
+} from 'src/app/services/interface.index';
 
 @Component({
   selector: 'app-vehiculos',
@@ -6,10 +19,202 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehiculos.component.scss']
 })
 export class VehiculosComponent implements OnInit {
-
-  constructor() { }
+  mMarcas:IMarcas[];
+  mConcesionarios:IConcesionario[];
+  mEstado: IEstados[];
+  submitted = false;
+  mVehiculos: IVehiculos[];
+  mForma: FormGroup;
+  mVehiculosSelect: IVehiculos;
+  mFormaEstado: string;
+  public loading = false;
+  Pkey: number;
+  constructor(
+    private modalService: NgbModal,
+    private service: VehiculosService,
+    private serviceMarcas: MarcasService,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService,
+    private state: ObjectService,
+  ) { 
+    this.mMarcas=[];
+    this.mConcesionarios=[];
+    this.mForma = this.generarFormulario();
+    this.mVehiculos=[];
+    this.mVehiculosSelect = Vehiculos.empy();
+    this.mFormaEstado = '4';
+    this.mEstado = this.state.Estados();
+    this.Pkey=0;
+  }
 
   ngOnInit(): void {
+    this.getAll();
+    this.getAllMarcas();
+    this.getAllConcesionarios();
   }
+
+
+
+  get f() { return this.mForma.controls; }
+
+  generarFormulario() {
+    return this.formBuilder.group({
+      Modelo: ['', Validators.required],
+      Precio: ['', Validators.required],
+      Color: ['', Validators.required],
+      Estado: ['', Validators.required],
+      Tipo: ['', Validators.required],
+      Traccion: ['', Validators.required],
+      TCMarcaId:  ['', Validators.required],
+      TCConcesionarioId:  ['', Validators.required],
+    });
+  }
+
+  nuevo(content: any) {
+    this.modalService.open(content, { size: 'lg' });
+    this.mFormaEstado ="1";
+  }
+  ver(content: any, pkey: number) {
+    this.getXId(pkey);
+    this.modalService.open(content, { size: 'lg',centered: true  });
+    this.mFormaEstado = '2';
+    this.mForma.disable();
+  }
+
+
+  
+  modificar(content: any, pkey: number) {
+    this.Pkey = pkey;
+    this.mForma.enable();
+    this.getXId(this.Pkey);
+    this.modalService.open(content, { size: 'lg',centered: true  });
+    this.mFormaEstado = '3';
+  }
+
+
+  getXId(pkey:number) {
+    this.loading = true;
+    this.service.AllXId(pkey).then(data => {
+      this.mForma.setValue({
+         Nombre: data[0].Nombre,
+        Estado: data[0].Estado
+      });
+      this.loading = false;
+    }).catch((error: { message: string | undefined; }) => {
+      this.loading = false;
+      this.toastr.error(error.message, "Vehiculos");
+    });
+  }
+
+  getAll() {
+    this.loading = true;
+    this.service.AllPage().then(data => {
+      this.mVehiculos = data;
+     this.loading = false;
+    }).catch(error => {
+      this.toastr.error(error.message, "Vehiculos");
+      this.loading = false;
+    });
+  }
+
+  
+  getAllMarcas() {
+    this.loading = true;
+    this.serviceMarcas.AllPage().then(data => {
+      this.mMarcas = data;
+     this.loading = false;
+    }).catch(error => {
+      this.toastr.error(error.message, "Marcas");
+      this.loading = false;
+    });
+  }
+
+
+  getAllConcesionarios() {
+    this.loading = true;
+    this.serviceMarcas.AllPage().then(data => {
+      this.mConcesionarios = data;
+     this.loading = false;
+    }).catch(error => {
+      this.toastr.error(error.message, "Concesionarios");
+      this.loading = false;
+    });
+  }
+
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.mForma.invalid) {
+      return;
+    } else {
+      this.mVehiculosSelect = this.mForma.value as IVehiculos;
+      if (this.mFormaEstado === '1') {
+        this.guardar();
+      } else if (this.mFormaEstado === '3') {
+        this.actualizar(this.Pkey);
+      }
+    }
+  }
+
+
+  guardar() {
+    this.loading = true;
+    this.service.New(this.mVehiculosSelect).then(data => {
+      this.toastr.success(data.message, "Vehiculos");
+      this.mFormaEstado = '4';
+      this.mVehiculos.unshift(data);
+      this.loading = false;
+   //   this.modalRef.close();
+   this.getDismissReason('');
+    }).catch(error => {
+      this.loading = false;
+      this.toastr.error(error.message, "Vehiculos");
+    });
+  }
+
+  actualizar(pKey: number) {
+    this.loading = true;
+    this.service.Update(this.mVehiculosSelect, pKey).then(data => {
+      this.toastr.success(data.message, "Vehiculos");
+      this.getDismissReason('');
+      this.mVehiculos = this.mVehiculos.map((object: IVehiculos) => {
+        if (object.id === pKey) {
+          return object = data;
+        } else {
+          return object;
+        }
+      });
+
+      this.loading = false;
+    }).catch(error => {
+      this.loading = false;
+      this.toastr.error(error.message, "Vehiculos");
+    });
+  }
+
+  
+  eliminar(pKey: number) {
+    this.loading = true;
+    this.service.Delete(pKey).then(data => {
+      console.log(data)
+      this.toastr.success(data.message, "Vehiculos");
+        this.mVehiculos = this.mVehiculos.filter((object: IVehiculos) => object.id !== pKey);
+      this.loading = false;
+    }).catch(error => {
+      this.loading = false;
+      this.toastr.error(error.message, "Vehiculos");
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
 
 }
--- src/app/services/vehiculos/vehiculos.service.ts
+++ src/app/services/vehiculos/vehiculos.service.ts
@@ -1,9 +1,76 @@
 import { Injectable } from '@angular/core';
+//import { ConfigService } from '../CF/config/config.service';
+import { HttpClient } from '@angular/common/http';
+import { getHeaders } from '../CF/misc/headers';
+import { map } from 'rxjs/operators';
+import { ServicesModule } from '../services.module';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: ServicesModule
 })
 export class VehiculosService {
 
-  constructor() { }
+  private mService = 'vehiculos';
+
+  private mUrl = 'http://localhost:3000/';
+  constructor(
+    private httpClient: HttpClient,
+
+  ) { }
+
+
+
+  async AllPage() {
+    console.log("ruta",this.mUrl + this.mService)
+    return await this.httpClient.get(this.mUrl + this.mService, {
+      headers: getHeaders()
+    }).pipe(
+      map((data: any) => {
+        console.log("resp",data)
+        return data;
+      })).toPromise();
+  }
+
+
+  async New(pDatos: any) {
+    const lDatos = JSON.stringify(pDatos);
+    console.log("ldatos",lDatos)
+    return await this.httpClient.post(this.mUrl + this.mService, lDatos, {
+      headers: getHeaders()
+    }).pipe(
+      map((data: any) => {
+        return data;
+      })).toPromise();
+  }
+
+
+
+  async AllXId(pKey: any) {
+    return await this.httpClient.get(this.mUrl + this.mService + '/' + pKey, {
+      headers: getHeaders()
+    }).pipe(
+      map((data: any) => {
+        return data;
+      })).toPromise();
+  }
+
+
+  async Update(pObj: any, pKey: number) {
+    const jObj = JSON.stringify(pObj);
+    return await this.httpClient.put(this.mUrl + this.mService + '/' + pKey, jObj,
+      { headers: getHeaders() })
+      // tslint:disable-next-line: arrow-return-shorthand
+      .pipe(map((data: any) => { return data; }))
+      .toPromise();
+  }
+
+
+  async Delete(pKey: number) {
+    return await this.httpClient.delete(this.mUrl + this.mService + '/' + pKey,
+      { headers: getHeaders() }).pipe(
+        map((data: any) => {
+          return data;
+        })).toPromise();
+  }
+
 }
